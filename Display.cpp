0b97ccff466c2d6462eb417987cf301c0ccb9dce
Mário <igotbugs@ticklemynausea.net>
2015-12-03 16:45:38 +0000

    display primitives and sketches for future code

--- Display.cpp
+++ Display.cpp
@@ -1,5 +1,6 @@
-#include "display.h"
-#include "dhtsensor.h"
+#include "Display.h"
+#include "Buttons.h"
+#include "DHTSensor.h"
 
 Adafruit_PCD8544 Display::controller = Adafruit_PCD8544(LCD_PIN_SCLK, LCD_PIN_DIN, LCD_PIN_DC, LCD_PIN_CS, LCD_PIN_RST);
 
@@ -13,53 +14,111 @@ void Display::Initialize() {
   Display::controller.begin();
   
   Display::controller.setContrast(50);
+
   Display::controller.setRotation(2);
 
   //Display::controller.display(); // show splashscreen
+  
   Display::controller.clearDisplay();   // clears the screen and buffer
 
-  //delay(2000);
+  Display::ShowMessage(F("   #IoT ;-)   "), F("Hi! Welcome to the thing!"));
+
+  Serial.println(F("## Hi! Welcome to the thing!"));
+
+}
+
+void Display::UpdateFromDisplay() {
+
+  Display::controller.display();
+  Display::updateCounter++;
+
+}
+
+void Display::Set(uint8_t x, uint8_t y) {
+
+  Display::controller.drawPixel(x, y, BLACK);
+
+}
+
+void Display::Unset(uint8_t x, uint8_t  y) {
+
+  Display::controller.drawPixel(x, y, WHITE);
+
+}
+
+void Display::DrawLine(uint8_t  x0, uint8_t  y0, uint8_t  x1, uint8_t  x2, uint8_t  color) {
+
+  Display::controller.drawLine(x0, y0, x1, x2, color);
+
+};
+
+void Display::DrawText(uint8_t x, uint8_t y, String &text) {
+
+  Display::controller.setCursor(x, y);
+  Display::controller.setTextColor(WHITE, BLACK);
+  Display::controller.print(text);
+
+};
+
+void Display::StatusScreen() {
+
+  Serial.println("Display::StatusScreen");
+  Display::controller.setTextSize(2);
+  
+  Display::controller.clearDisplay();   // clears the screen and buffer
+  Display::controller.setCursor(0,0);
+  Display::controller.setTextColor(WHITE, BLACK); // 
+  Display::controller.println(F("LOL"));
+
+  Display::UpdateFromDisplay();
+
+  while (Buttons::IsPressed(GPIO_BUTTON_A));
 }
 
 void Display::TemperatureScreen() {
-    Display::controller.setTextSize(1);
-    
-    Display::controller.clearDisplay();   // clears the screen and buffer
-    Display::controller.setCursor(0,0);
-    Display::controller.setTextColor(WHITE, BLACK); // 'inverted' text
-    Display::controller.println(F(" #IoT FTW :-) "));
-    Display::controller.setTextColor(BLACK, WHITE);
-    Display::controller.print(F("Temp:  "));
-    Display::controller.println(String(DHTSensor::temperatureValue) + F(" C"));
-    Display::controller.print(F("Humi:  "));
-    Display::controller.println(String(DHTSensor::humidityValue) + F(" %"));
+  
+  Display::controller.setTextSize(1);
+  
+  Display::controller.clearDisplay();   // clears the screen and buffer
+  Display::controller.setCursor(0,0);
+  Display::controller.setTextColor(WHITE, BLACK); // 'inverted' text
+  Display::controller.println(F(" #IoT FTW :-) "));
+  Display::controller.setTextColor(BLACK, WHITE);
+  Display::controller.print(F("Temp:  "));
+  Display::controller.println(String(DHTSensor::temperatureValue) + F(" C"));
+  Display::controller.print(F("Humi:  "));
+  Display::controller.println(String(DHTSensor::humidityValue) + F(" %"));
 
-    for (int i=0; i <= Display::updateCounter % 6; i++) {
-      Display::controller.drawPixel((84/2)+(i*2), 44, BLACK);
-      Display::controller.drawPixel((84/2)-(i*2), 44, BLACK);
-      Display::controller.drawPixel((84/2)+(i*2), 45, BLACK);
-      Display::controller.drawPixel((84/2)-(i*2), 45, BLACK);
-    }
+  for (int i=0; i <= Display::updateCounter % 6; i++) {
+    Display::controller.drawPixel((84/2)+(i*2), 44, BLACK);
+    Display::controller.drawPixel((84/2)-(i*2), 44, BLACK);
+    Display::controller.drawPixel((84/2)+(i*2), 45, BLACK);
+    Display::controller.drawPixel((84/2)-(i*2), 45, BLACK);
+  }
 
-    Display::controller.setCursor(3,0);
+  //Display::controller.setCursor(3,0);
 
-    Display::controller.display();
-    Display::updateCounter++;
+  Display::UpdateFromDisplay();
+
 }
 
 
 void Display::ShowMessage(String title, String message, bool clearDisplay) {
 
-    Display::controller.setTextSize(1);
+  Display::controller.setTextSize(1);
 
-    if (clearDisplay) {
-      Display::controller.clearDisplay();   // clears the screen and buffer
-    }
+  if (clearDisplay) {
+    Display::controller.clearDisplay();   // clears the screen and buffer
+  }
 
-    Display::controller.setCursor(0,0);
-    Display::controller.setTextColor(WHITE, BLACK); // 'inverted' text
-    Display::controller.println(title);
-    Display::controller.setTextColor(BLACK, WHITE);
-    Display::controller.println(message);
-    Display::controller.display();
+  Display::controller.setCursor(0,0);
+  Display::controller.setTextColor(WHITE, BLACK); // 'inverted' text
+  Display::controller.println(title);
+  Display::controller.setTextColor(BLACK, WHITE);
+  Display::controller.println(message);
+  Display::controller.display();
+
+  Display::UpdateFromDisplay();
+
 }
+
--- Display.h
+++ Display.h
@@ -4,12 +4,35 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 
-/* Nokia LCD Pinout */
+/* Phsyical circuit connections */
 #define LCD_PIN_SCLK 9
 #define LCD_PIN_DIN 8
 #define LCD_PIN_DC 5
 #define LCD_PIN_CS 6
 #define LCD_PIN_RST 7
+
+/* LCD characteristics */
+#define LCD_WIDTH 84
+#define LCD_HEIGHT 48
+#define LCD_HEIGHT8 (LCD_HEIGHT >> 3)
+
+/* * set/unset */
+#define PIXEL_SET(x, y) Display::controller.drawPixel(x, y, BLACK);
+#define PIXEL_UNSET(x, y) Display::controller.drawPixel(x, y, WHITE);
+
+/* * checks if n is in the interval [a, b] * */
+#define BETWEEN(n, a, b) ((n >= a) && (n <= b))
+
+/* * over matrix buffer width */
+#define FOR_i for (uint8_t i = 0; i < LCD_WIDTH; i++)
+/* * over matrix buffer height */
+#define FOR_j for (uint8_t j = 0; j < LCD_HEIGHT8; j++)
+
+/* * over screen width * */
+#define FOR_x for (uint8_t x = 0; x < LCD_WIDTH; x++)
+
+/* * over screen height * */
+#define FOR_y for (uint8_t y = 0; y < LCD_HEIGHT; y++)
 
 class Display {
 
@@ -20,10 +43,18 @@ class Display {
   static long lastUpdate;
 
   static int updateCounter;
-  
+
+  /* Helpers */
+  static void Set(uint8_t  x, uint8_t y);
+  static void Unset(uint8_t  x, uint8_t y);
+  static void DrawLine(uint8_t x0, uint8_t  y0, uint8_t  x1, uint8_t  x2, uint8_t color);
+  static void DrawText(uint8_t x, uint8_t y, String &text);
+  static void UpdateFromDisplay();
+
   /* Initialize the LED display */
   static void Initialize();
   static void TemperatureScreen();
+  static void StatusScreen();
   static void ShowMessage(String title, String message, bool clearDisplay = true);
 
 };
